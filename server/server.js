39ba59e197cf0ebcf58ffeb3fef24a6437aa68ea
ahf998 <arashfiroozi32@gmail.com>
2018-12-04 10:12:20 +0330

    index.html served

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require ('express');
+const path = require('path');
+
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-
+    res.sendFile(path.join(__dirname, '../static/index.html'));
 });
 
-app
+app.listen(port, () => {
+   console.log(`app listening on port ${port}`);
+});
