f20122630092b7458169dc679370434d016e3cf5
ahf998 <arashfiroozi32@gmail.com>
2018-12-04 11:04:16 +0330

    database added and seved one model to it

--- server/server.js
+++ server/server.js
@@ -1,8 +1,36 @@
 const express = require ('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/blogdb');
 
 const app = express();
 const port = 8080;
+
+var blogpost = mongoose.model('blogpost', {
+    text: {
+     type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    postedat:{
+        type: Number
+    }
+});
+
+var newblogpost = new blogpost({
+    text: "first post in this blog and database",
+    email: "arashfiroozi32@gmail.com"
+});
+
+newblogpost.save().then((doc)=> {
+    console.log('creating model success', doc);
+}, (e) => {
+    console.log('unable to save data');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../static/index.html'));
